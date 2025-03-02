e610dd4be74ee1d7619bb5be4486a76e822fc7b5
suvamneog <neogsuvam@gmail.com>
2025-03-03 00:21:50 +0530

    ADDED

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { Routes, Route } from "react-router-dom"
 import { AuthProvider } from "./utils/AuthContext"
 import ShootingStarsAndStarsBackgroundDemo from "./pages/background"
+import Navbar from "./pages/Navbar";
 import AddFood from "./pages/addFood"
 import LogMeals from "./pages/logMeals"
 import SignupFormDemo from "./pages/signup"
@@ -12,7 +13,7 @@ import History from "./pages/history"
 import  Text from "./pages/Text"
 import BarcodeScanner from "./pages/barcodeScanner"
 import FoodScanner from "./pages/FoodImageRecognition"
-import Navbar from './pages/Navbar';
+
 
 
 function App() {
